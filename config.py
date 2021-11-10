d2d72c6ea58ecf75468d15dff26d37b630805f40
Nico Pinguel <nico.mp215@gmail.com>
2021-11-09 21:14:27 -0600

    added .env

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
-
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Give access to the project in any OS we find ourslves in
 # Allow outside files/folders to be added to the project
 # base directory
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     """
@@ -12,7 +13,8 @@ class Config():
         Using Environment vvaiales where available
         create config variables if not done already
     """
-
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY')or 'You will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' +os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
